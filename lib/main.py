0a0568c7f8bbc1321566eaf3a91f15dd37aa33d2
GaGandour <gagandour@gmail.com>
2022-03-27 21:24:39 -0300

    two foods

--- lib/main.py
+++ lib/main.py
@@ -1,8 +1,10 @@
 import pygame, sys, os
 sys.path.append(os.path.join(sys.path[0],'pages'))
+sys.path.append(os.path.join(sys.path[0],'objects'))
 
 from settings import *
 
+import chinelao, lagarto
 from ranch import Ranch
 from home_page import Home_Page
 
@@ -14,6 +16,7 @@ class Game:
         pygame.display.set_caption('Hora do Rancho')
         self.level = 0
         self.screen_name = Home_Page.page_name
+        self.food_names = [chinelao.Chinelao.food_name, lagarto.Lagarto.food_name]
 
     def run(self):
         self.page = Home_Page(self.screen, self.level,self.change_screen)
@@ -29,9 +32,12 @@ class Game:
     def change_screen(self, screen_name):
         self.screen_name = screen_name
         self.page = {
-            Ranch.page_name : Ranch(self.screen, self.level),
+            Ranch.page_name : Ranch(self.screen, self.level, self.food_names),
             Home_Page.page_name : Home_Page(self.screen, self.level, self.change_screen),
-        }.get(screen_name, Ranch.page_name)
+        }.get(screen_name, Home_Page.page_name)
+
+    def add_food(self, food_name):
+        self.food_names.append(food_name)
 
 
 if __name__ == '__main__':
--- lib/pages/ranch.py
+++ lib/pages/ranch.py
@@ -1,7 +1,10 @@
 import pygame, sys, os
+from random import choice
+
+from chinelao import Chinelao
+from lagarto import Lagarto
 sys.path.append('./')
 sys.path.append(os.path.join(sys.path[0], 'objects'))
-from food import Food
 sys.path.append(os.path.join(sys.path[0], 'player'))
 from lib.player.player import Player
 from settings import *
@@ -9,23 +12,23 @@ from settings import *
 class Ranch:
     page_name = "ranch"
 
-    def __init__(self, screen, level):
+    def __init__(self, screen, level, food_names):
         self.map_sprite = pygame.transform.scale(pygame.image.load('./assets/maps/scenery.jpg').convert(), (WIDTH, HEIGHT))
         self.level = level
         self.screen = screen
-
+        self.food_names = food_names
         self.player = pygame.sprite.GroupSingle()
         self.player.add(Player())
         
         self.foods = pygame.sprite.Group()
         self.food_timer = pygame.USEREVENT + 1
-        pygame.time.set_timer(self.food_timer, 300)
+        pygame.time.set_timer(self.food_timer, 100)
 
     def update(self):
         pygame.display.update()
         for event in pygame.event.get():
                 if event.type == self.food_timer:
-                    self.foods.add(Food(CHINELAO_NAME))
+                    self.foods.add(self.get_food())
         
         self.screen.blit(self.map_sprite,(0,0))
         
@@ -33,6 +36,12 @@ class Ranch:
         self.player.update()
         self.foods.draw(self.screen)
         self.foods.update()
+
+    def get_food(self):
+        return {
+            Chinelao.food_name : Chinelao(),
+            Lagarto.food_name : Lagarto(),
+        }.get(choice(self.food_names), Chinelao.food_name)
 
 
 
