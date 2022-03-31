17a468fd8a31e0ec41515ec3dcb12ac782c4d4f2
Emmanuel Dias <emanusdias@gmail.com>
2022-03-31 13:02:21 -0300

    changes on health, new ui class, basic gameover

--- lib/player/player.py
+++ lib/player/player.py
@@ -4,7 +4,7 @@ from settings import *
 from support import import_folder
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, screen):
+    def __init__(self,change_health,change_sickness):
         super().__init__()
         self.import_character_assets()
         self.frame_index = 0
@@ -12,26 +12,15 @@ class Player(pygame.sprite.Sprite):
         self.image = self.animations['down'][self.frame_index]
         self.rect = self.image.get_rect(center = (0.5 * WIDTH, 0.5 * HEIGHT))
 
-        # ui setup
-        self.screen = screen
-
         # player movement
         self.direction = pygame.math.Vector2(0,0)
-        self.speed = 3
+        self.speed = 4
         self.ismoving = False
-        self.status = 'down'
+        self.stance = 'down'
 
-        # player health and sickness
-        self.alive = True
-        self.life = 100
-        self.sick = 0
-        self.max_life = 100
-        self.max_sick = 100
-        self.length_of_bars = 0.5 * WIDTH
-        self.ratio_life_to_bar = self.length_of_bars / self.max_life
-        self.ratio_sick_to_bar = self.length_of_bars / self.max_sick
-        self.life_rect = pygame.Rect(0.25*WIDTH,25,self.length_of_bars,20)
-        self.sick_rect = pygame.Rect(0.25*WIDTH,50,self.length_of_bars,20)
+        # health/sickness management
+        self.change_health = change_health
+        self.change_sickness = change_sickness
 
     def import_character_assets(self):
         # set paths and store sprites
@@ -64,24 +53,24 @@ class Player(pygame.sprite.Sprite):
             self.ismoving = True
 
         # constrain to ranch
-        if self.rect.bottom > HEIGHT:
-            self.rect.bottom = HEIGHT
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.top < 0:
-            self.rect.top = 0    
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
+        if self.rect.bottom > HEIGHT-40:
+            self.rect.bottom = HEIGHT-40
+        if self.rect.left < 40:
+            self.rect.left = 40
+        if self.rect.top < 100:
+            self.rect.top = 100    
+        if self.rect.right > WIDTH-40:
+            self.rect.right = WIDTH-40
 
-    def get_status(self):
+    def get_stance(self):
         if self.direction.x > 0:
-            self.status = 'right'
+            self.stance = 'right'
         elif self.direction.x < 0:
-            self.status = 'left'
+            self.stance = 'left'
         elif self.direction.y > 0:
-                self.status = 'down'
+                self.stance = 'down'
         elif self.direction.y < 0:
-            self.status = 'up'
+            self.stance = 'up'
 
     def walk(self):
         if self.ismoving:
@@ -93,7 +82,7 @@ class Player(pygame.sprite.Sprite):
                 self.rect.y += round(self.speed * 0.707 * self.direction.y)
 
     def animation_state(self):
-        animation = self.animations[self.status]
+        animation = self.animations[self.stance]
 
         # loop frame index 
         self.frame_index += self.animation_speed
@@ -104,29 +93,20 @@ class Player(pygame.sprite.Sprite):
             self.image = animation[int(self.frame_index)]
         else: self.image = animation[0]
 
-    def draw_ui(self):
-        # draw health
-        pygame.draw.rect(self.screen,(255,0,0),(0.25*WIDTH,25,self.ratio_life_to_bar*self.life,20))
-        pygame.draw.rect(self.screen,(255,255,255),self.life_rect,4)
-        # draw sickness
-        pygame.draw.rect(self.screen,(0,255,0),(0.25*WIDTH,50,self.ratio_sick_to_bar*self.sick,20))
-        pygame.draw.rect(self.screen,(255,255,255),self.sick_rect,4)
+    def decay_self(self): 
+        self.change_health(-0.1)
+        self.change_sickness(-0.05)
 
-    def check_health(self): 
-        self.life -= 0.1
-        self.sick += 0.1
-        if self.life <= 0:
-            self.live = 0
-            self.alive = False
-        if self.sick >= self.max_sick:
-            self.sick = self.max_sick
-            self.alive = False
+    def ate_good_food(self):
+        self.change_health(+20)
     
+    def ate_bad_food(self):
+        self.change_sickness(+40)
+        
     def update(self):
-        self.check_health()
-        self.draw_ui()
+        self.decay_self()
         self.get_input()
-        self.get_status()
+        self.get_stance()
         self.walk()
         self.animation_state()
 
---
+++ lib/ui.py
@@ -0,0 +1,39 @@
+import pygame
+
+class UI:
+    def __init__(self,screen):
+        # setup 
+        self.display_surface = screen 
+
+        # health and sickness
+        self.health_bar = pygame.image.load('./assets/images/ui/health_bar.png').convert_alpha()
+        self.health_bar_topleft = (309,23)
+        self.sickness_bar = pygame.image.load('./assets/images/ui/sickness_bar.png').convert_alpha()
+        self.sickness_bar_topleft = (309,60)
+        self.bar_max_width = 344
+        self.bar_height = 14
+
+        # clock 
+        self.clock = pygame.image.load('./assets/images/ui/clock.png').convert_alpha()
+        self.clock_rect = self.clock.get_rect(topleft = (800,20))
+        self.font = pygame.font.Font('./assets/images/ui/ARCADEPI.ttf',30)
+
+    def show_health(self,current,full):
+        self.display_surface.blit(self.health_bar,(277,0))
+        current_health_ratio = current / full
+        current_bar_width = self.bar_max_width * current_health_ratio
+        health_bar_rect = pygame.Rect(self.health_bar_topleft,(current_bar_width,self.bar_height))
+        pygame.draw.rect(self.display_surface,'#dc4949',health_bar_rect)
+    
+    def show_sickness(self,current,full):
+        self.display_surface.blit(self.sickness_bar,(277,37))
+        current_sickness_ratio = current / full
+        current_bar_width = self.bar_max_width * current_sickness_ratio
+        sickness_bar_rect = pygame.Rect(self.sickness_bar_topleft,(current_bar_width,self.bar_height))
+        pygame.draw.rect(self.display_surface,'#4fbd65',sickness_bar_rect)
+
+    def display_time(self,time):
+        self.display_surface.blit(self.clock,self.clock_rect)
+        time_surf = self.font.render(str(time),False,'#ffffff')
+        time_rect = time_surf.get_rect(midleft = (self.clock_rect.right + 4,self.clock_rect.centery))
+        self.display_surface.blit(time_surf,time_rect)
