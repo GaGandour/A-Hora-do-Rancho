617a44d39b756892fe30f19a3e619e970e256c3c
GaGandour <gagandour@gmail.com>
2022-04-04 17:04:39 -0300

    fixed gameover page

--- lib/objects/food.py
+++ lib/objects/food.py
@@ -1,10 +1,29 @@
 import pygame, sys
 sys.path.append('./')
 from settings import *
+from random import choice
 
 class Food(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        (self.x_spawn, self.y_spawn, self.x_speed, self.y_speed) = choice([
+            (-10,-10, 4, 4),
+            (WIDTH+10,-10, -4, 4),
+            (-10,HEIGHT+10, 4, -4),
+            (WIDTH+10,HEIGHT+10, -4, -4),
+            (WIDTH/3,-10, 0, 4),
+            (WIDTH/3,HEIGHT+10, 0, 4),
+            (2*WIDTH/3,-10, 0, 4),
+            (2*WIDTH/3,HEIGHT+10, 0, -4),
+            (-10,HEIGHT/3, 4, 0),
+            (WIDTH+10,HEIGHT/3, -4, 0),
+            (-10,2*HEIGHT/3, 4, 0),
+            (WIDTH+10,2*HEIGHT/3, -4, 0),
+            (WIDTH/2,HEIGHT+10, 0, -4),
+            (WIDTH/2,-10, 0, 4),
+            (WIDTH+10,HEIGHT/2, -4, 0),
+            (-10,HEIGHT/2, 4, 0),
+        ])
     
     def update(self):
         self.destroy()
