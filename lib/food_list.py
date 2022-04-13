b292659841d70d1dc6942ba12d7dc33be4ac1743
Emmanuel Dias <emanusdias@gmail.com>
2022-04-13 09:08:46 -0300

    added 4 new foods (2 levels)

--- lib/food_list.py
+++ lib/food_list.py
@@ -5,10 +5,18 @@ from chinelao import Chinelao
 from lagarto import Lagarto
 from costelinha import Costelinha
 from frango_assado import Frango_Assado
+from camarao import Camarao
+from moqueca import Moqueca
+from salsicha import Salsicha
+from sushi import Sushi
 
 FOOD_LIST = [
     Costelinha,
     Frango_Assado,
     Chinelao,
     Lagarto,
+    Moqueca,
+    Salsicha,
+    Camarao,
+    Sushi
 ]
