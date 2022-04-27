5ca5ce1c9c9b8aed5f3766ddf683ca3d76492977
GaGandour <gagandour@gmail.com>
2022-04-27 20:09:44 -0300

    minecraft reference (pulsating text in homepage)

--- lib/pages/home_page.py
+++ lib/pages/home_page.py
@@ -5,8 +5,9 @@ sys.path.append(os.path.join(sys.path[0], 'objects'))
 sys.path.append(os.path.join(sys.path[0], 'widgets'))
 from settings import *
 from menu_button import Menu_Button
+from customized_text import Customized_Text
 from math import sin, cos
-
+from random import choice
 
 
 class Home_Page:
@@ -18,10 +19,21 @@ class Home_Page:
         self.title = pygame.transform.scale2x(pygame.image.load('./assets/images/home_page/title.png').convert_alpha())
         self.moving_bg = pygame.transform.scale2x(pygame.image.load('./assets/images/home_page/moving_bg.png').convert_alpha())
         self.screen = screen
+
+        random_text = choice([
+            'Almoco gratis!',
+            'Soh Iguaria!',
+            'Melhor lugar para comer!',
+            '9 a cada 10 estudantes recomendam!',
+            'Uhuuuuu!!!',
+            'Trouxe o crachah?',
+            '"Moqueca de novo???"',
+        ])
         
         self.buttons = [
             Menu_Button(screen, (480, 274), "Play", lambda: change_screen(Food_Choice.page_name)),
             Menu_Button(screen, (480, 422), "How To Play", lambda: change_screen(Food_Choice.page_name)),
+            Customized_Text(screen, (780, 190), random_text, size=20, color='Yellow', rotation=20, pulse=True),
         ]
 
     def update(self):
--- lib/widgets/customized_text.py
+++ lib/widgets/customized_text.py
@@ -1,19 +1,32 @@
 import pygame
+from math import sin
 
 class Customized_Text:
-    def __init__(self, screen, position, text, size = 50, color = 'White', rotation = 0, function = lambda : None):
+    def __init__(self, screen, position, text, size = 50, color = 'White', rotation = 0, function = lambda : None, pulse = False):
         # setup 
         self.screen = screen
         self.function = function
         
         text_font = pygame.font.Font('./assets/fonts/ARCADEPI.ttf',size)
         self.text_surf = text_font.render(text, False, color)
+        self.text_surf = pygame.transform.rotate(self.text_surf, rotation)
         self.text_rect = self.text_surf.get_rect(center = position)
-        self.rotation = rotation
         self.was_pressed = False
+        self.pulse = pulse
+        self.position = position
+        self.color = color
+        self.rotation = rotation
+        self.size = size
+        self.text = text
 
 
     def update(self):
+        if self.pulse:
+            text_font = pygame.font.Font('./assets/fonts/ARCADEPI.ttf',self.size + int(sin(pygame.time.get_ticks()*0.006)*6))
+            self.text_surf = text_font.render(self.text, False, self.color)
+            self.text_surf = pygame.transform.rotate(self.text_surf, self.rotation)
+            self.text_rect = self.text_surf.get_rect(center = self.position)
+            
         self.screen.blit(self.text_surf, self.text_rect)
 
         if (not pygame.mouse.get_pressed()[0]) and self.text_rect.collidepoint(pygame.mouse.get_pos()):
