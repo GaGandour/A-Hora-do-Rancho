fd2ac424b55b9971995d403ad864932fbf0139df
asclaudino <arielsclaudino@gmail.com>
2022-04-28 13:49:36 -0300

    Added backgrounds animatinos to game over and win page

--- lib/pages/game_over.py
+++ lib/pages/game_over.py
@@ -5,6 +5,8 @@ sys.path.append(os.path.join(sys.path[0], 'widgets'))
 from settings import *
 from customized_text import Customized_Text
 from menu_button import Menu_Button
+from math import sin, cos
+
 
 class Game_Over:
     page_name = "game over"
@@ -12,10 +14,13 @@ class Game_Over:
     def __init__(self, screen, go_back_to_home_function):
         #self.map_sprite = pygame.transform.scale(pygame.image.load('./assets/maps/scenery.jpg').convert(), (WIDTH, HEIGHT))
         self.bg_surface = pygame.Surface((WIDTH,HEIGHT))
-        self.bg_surface.fill("#2e2e2e")
+        self.bg_surface.fill("#222222")
         self.screen = screen
+        self.background = pygame.transform.scale(pygame.image.load('./assets/images/home_page/title_bg.png').convert(), (WIDTH, HEIGHT))
+        self.moving_bg = pygame.transform.scale2x(pygame.image.load('./assets/images/home_page/moving_bg.png').convert_alpha())
         
         self.buttons = [
+             Customized_Text(screen, (WIDTH/2+4, 190+4), "Game Over!!!", size = 100, color='Black'),
             Customized_Text(screen, (WIDTH/2, 190), "Game Over!!!", size = 100, color='Red'),
             Menu_Button(screen, (WIDTH/2, 350), "Back to Menu", go_back_to_home_function),
         ]
@@ -25,6 +30,8 @@ class Game_Over:
         pygame.display.update()
         
         self.screen.blit(self.bg_surface,(0,0))
+        #self.screen.blit(self.background,(0,0))
+        self.screen.blit(self.moving_bg,(-250+50*cos(pygame.time.get_ticks()*0.0005),-200+50*sin(pygame.time.get_ticks()*0.0005)))
 
         if self.buttons:
             for button in self.buttons:
