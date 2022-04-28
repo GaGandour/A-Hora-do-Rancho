a4a64ca8f0fda522dec63963e8aeb4692704f505
GaGandour <gagandour@gmail.com>
2022-04-28 09:25:05 -0300

    button hover animation

--- lib/game_test.py
+++ lib/game_test.py
@@ -8,9 +8,10 @@ from main import Game
 import pygame
 
 class Game_Test(Game):
-    def __init__(self, initial_page_name, level = 1):
+    def __init__(self, initial_page_name, level = 1, mute = True):
         super().__init__()
         self.level = level
         assert(initial_page_name != Ranch.page_name)
         self.change_screen(initial_page_name)
-        pygame.mixer.music.set_volume(0)
+        if mute:
+            pygame.mixer.music.set_volume(0)
