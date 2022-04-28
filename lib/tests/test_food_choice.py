c1578e5f666a44c0c81bfabf4e5966a867a5d5b7
GaGandour <gagandour@gmail.com>
2022-04-28 15:43:38 -0300

    configured music in blackout

--- lib/tests/test_food_choice.py
+++ lib/tests/test_food_choice.py
@@ -10,5 +10,5 @@ from food_choice import Food_Choice
 from game_test import Game_Test
 
 if __name__ == '__main__':
-    game = Game_Test(Food_Choice.page_name, level = 1)
+    game = Game_Test(Food_Choice.page_name, level = 3, mute=False)
     game.run()
